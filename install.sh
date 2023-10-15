b4337302c8493e269f264416da9fd4117b4d8309
ahhfzwl <ahhfzwl@qq.com>
2023-10-15 20:40:48 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
-apt update && apt install -y wget curl vim
-mkdir /root/sing-box-warp/ && cd /root/sing-box-warp/
-wget -O /root/sing-box-warp/sing-box https://ahzwl.tk/sing-box/sing-box
-wget -O /root/sing-box-warp/config.json https://ahzwl.tk/sing-box/config.json
-wget -O /etc/systemd/system/sing-box-warp.service https://ahzwl.tk/sing-box/sing-box-warp.service
-chmod +x /root/sing-box-warp/sing-box
-systemctl restart sing-box-warp # 重启服务
-systemctl enable sing-box-warp # 开机启动
+#!/bin/bash -e
+apt update && apt install -y openssh-server systemctl wget curl vim nano screen unzip htop
+mkdir /root/sing-box/ && cd /root/sing-box/
+wget https://github.sock.cf/SagerNet/sing-box/releases/download/v1.5.3/sing-box-1.5.3-linux-amd64.tar.gz
+tar -xvf sing-box-*.tar.gz
+cp ./sing-box-*/sing-box /root/sing-box/
+rm -rf /root/sing-box/sing-box-*
+chmod +x /root/sing-box/sing-box
+wget -O /root/sing-box/config.json https://raw.sock.cf/ahhfzwl/my/main/config.json
+wget -O /root/sing-box/systemctl.sh https://raw.sock.cf/ahhfzwl/my/main/systemctl.sh
+wget -O /etc/systemd/system/sing-box.service https://raw.sock.cf/ahhfzwl/my/main/sing-box.service
+systemctl enable sing-box # 开机启动
