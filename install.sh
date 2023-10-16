f52162dad48ec494f8b49bcec2ca134104b2f267
ahhfzwl <ahhfzwl@qq.com>
2023-10-16 18:04:12 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ tar -xvf sing-box-*.tar.gz
 cp ./sing-box-*/sing-box /root/sing-box/
 rm -rf /root/sing-box/sing-box-*
 chmod +x /root/sing-box/sing-box
-wget -O /root/sing-box/config.json https://raw.sock.cf/ahhfzwl/my/main/config.json
+wget -O /root/sing-box/config.json $config_url
 wget -O /etc/systemd/system/sing-box.service https://raw.sock.cf/ahhfzwl/my/main/sing-box.service
 systemctl enable sing-box
 wget -O /root/sing-box/systemctl.sh https://raw.sock.cf/ahhfzwl/my/main/systemctl.sh
