4f9b3da99fde6466233c97da6b61aa91b9585d16
ahhfzwl <ahhfzwl@qq.com>
2023-10-17 09:51:11 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,13 +5,17 @@ echo 1.跳过
 echo 2.安装
 echo -e 0.退出'\n'
 read -p "请选择(默认1):" mode
-if [ -z "$mode" ], then
+if [ -z "$mode" ]
+then
 	mode=1
 fi
-if [[ $mode == 1 ]], then
-elif [[ $mode == 2 ]], then
+if [ $mode == 1 ]
+then
+elif [ $mode == 2 ]
+then
   apt update && apt install -y wget unzip
-elif [[ $mode == 0 ]], then
+elif [ $mode == 0 ]
+then
 fi
 
 if [[ -z "$config" ]], then
