598ccf6b6b055d6e9588a20d61849e581c3b7a6b
ahhfzwl <ahhfzwl@qq.com>
2023-10-21 11:07:30 +0800

    Update systemctl.sh

--- systemctl.sh
+++ systemctl.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+
+colo=$1
 while true
 do
 meta=$(curl -x socks5://127.0.0.1:1080 -s https://speed.cloudflare.com/meta --connect-timeout 2 --max-time 3 | awk -F\" '{print $22}')
-if [ "$meta" != "HKG" ]
-then
-	echo $meta......
+if [[ "$meta" != "HKG" ]]; then
+ 	echo "$(date) $meta......"
 	systemctl restart sing-box
-	sleep 1
+	sleep 3
+elif [[ "$meta" == "HKG" ]]; then
+ 	echo "$(date) $meta......"
+	sleep 300
 else
-	echo $meta true
+	echo $meta
 	break
 fi
 done
