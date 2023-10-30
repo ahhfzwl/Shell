7b2cf3d4b1f67fd89714b863959e7993ec1b03b5
ahhfzwl <ahhfzwl@qq.com>
2023-10-30 17:43:01 +0800

    Update systemctl.sh

--- systemctl.sh
+++ systemctl.sh
@@ -5,18 +5,17 @@ if [[ -z "$colo" ]]; then
   colo=HKG
 fi
 
-while true
-do
-meta=$(curl -x socks5://127.0.0.1:1080 -s https://speed.cloudflare.com/meta --connect-timeout 2 --max-time 3 | awk -F\" '{print $22}')
-if [[ "$meta" != "$colo" ]]; then
- 	echo "$(date) $meta......"
-	systemctl restart sing-box
-	sleep 3
-elif [[ "$meta" == "$colo" ]]; then
- 	echo "$(date) $meta......"
-	sleep 300
-else
-	echo $meta
-	break
-fi
+i=1
+while [ $i -le 30 ]
+do   
+  meta=$(curl -x socks5://127.0.0.1:1080 -s https://speed.cloudflare.com/meta --connect-timeout 2 --max-time 3 | awk -F\" '{print $22}')
+  if [[ "$meta" != "$colo" ]]; then
+    echo "$(date) $meta......"
+    systemctl restart sing-box
+    sleep 3
+  else
+    echo "$(date) $meta"
+    break
+  fi
+  let i++
 done
