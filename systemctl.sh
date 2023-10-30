73964eff3da14f25acb85d217c422e52a8295233
ahhfzwl <ahhfzwl@qq.com>
2023-10-30 15:31:54 +0800

    Update systemctl.sh

--- systemctl.sh
+++ systemctl.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 colo=$1
-
 if [[ -z "$colo" ]]; then
   colo=HKG
 fi
 
-echo "开始连接 $colo......"
 while true
 do
 meta=$(curl -x socks5://127.0.0.1:1080 -s https://speed.cloudflare.com/meta --connect-timeout 2 --max-time 3 | awk -F\" '{print $22}')
