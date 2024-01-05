23c44729b57ee99f9afdf57e7341ba90f5309167
ahhfzwl <ahhfzwl@qq.com>
2024-01-05 15:34:23 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,11 +4,13 @@ install() {
   APK=
   for i in cron htop openssh-server wget curl nano screen iputils-ping net-tools dnsutils iproute2 tcptraceroute bc socat systemctl
   do
-    if [ -z `type -P $i` ]; then
+    if [ -z `type -P $i` ]
+    then
       APK="$APK $i"
     fi
   done
-  if [ -z $APK ]; then
+  if [ -z $APK ]
+  then
     echo "APK OK"
   else
     apt update && apt -y install $APK
