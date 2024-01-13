8a3318ddfb8e80e0168ff1d01c97786862c56a31
ahhfzwl <ahhfzwl@qq.com>
2024-01-13 15:07:42 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # bash <(curl -Ls https://raw.sock.cf/ahhfzwl/Shell/main/install.sh)
 install() {
   APK=""
-  for i in cron htop ssh nano wget curl screen iputils-ping net-tools dnsutils iproute2 tcptraceroute bc socat systemctl
+  for i in cron htop openssh-server nano wget curl screen iputils-ping net-tools dnsutils iproute2 tcptraceroute bc socat systemctl
   do
     if ! dpkg -l | grep -q "^ii  $i "; then
       APK="$APK $i"
