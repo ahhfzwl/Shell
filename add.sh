00167215fa59230c1717f6b862947a319d53d258
ahhfzwl <ahhfzwl@qq.com>
2024-01-13 14:45:33 +0800

    Update add.sh

--- add.sh
+++ add.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# bash <(curl -Ls https://raw.sock.cf/ahhfzwl/Shell/main/add.sh)
 # 定义要安装的软件列表
 software_list=("cron" "htop" "ssh" "nano" "wget" "curl" "screen" "iputils-ping" "net-tools" "dnsutils" "iproute2" "tcptraceroute" "bc" "socat" "systemctl")
 
